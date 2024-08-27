0f44c627db45e1102be7668a6c131b930397f655
Joel Rioflorido <92565672+2peng23@users.noreply.github.com>
2024-08-27 09:25:32 +0800

    Update passport.js

--- backend/auth/passport.js
+++ backend/auth/passport.js
@@ -12,7 +12,7 @@ passport.use(
     {
       clientID: id,
       clientSecret: secret,
-      callbackURL: "http://localhost:5555/api/auth/google/callback",
+      callbackURL: "https://mern-fulltack-2z01vp8v0-riorios-projects.vercel.app/api/auth/google/callback",
       passReqToCallback: true,
     },
     async (request, accessToken, refreshToken, profile, done) => {
