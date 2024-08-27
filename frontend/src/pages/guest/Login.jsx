d46a11cc6368e21c66531437d2f0b0a7a234c26f
Joel Rioflorido <92565672+2peng23@users.noreply.github.com>
2024-08-27 09:30:15 +0800

    Update Login.jsx

--- frontend/src/pages/guest/Login.jsx
+++ frontend/src/pages/guest/Login.jsx
@@ -33,7 +33,7 @@ const Login = () => {
 
   const googleLogin = () => {
     // Redirect to your Express server's Google OAuth2 endpoint
-    window.location.href = "http://localhost:5555/api/auth/google";
+    window.location.href = "https://mern-fulltack-2z01vp8v0-riorios-projects.vercel.app/api/auth/google";
   };
 
   return (
